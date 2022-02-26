3aebf3e6e926a779bb77eb5772c6e295b710e8b6
deaddeal00 <99400014+deaddeal00@users.noreply.github.com>
2022-02-26 18:12:27 +0530

    Add files via upload

---
+++ conact.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$date= $_POST['date'];
+$message= $_POST['message'];
+$to = "sreejithn875@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  date = " . $date . "\r\n Message =" . $message;
+$headers = "From: sreejiththottuvazhiyil@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
